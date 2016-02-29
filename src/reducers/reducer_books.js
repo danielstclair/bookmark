2171382bdedaa81e856422b99c436542d0ffe3b8
DANIEL A. ST CLAIR <danielstclair@us.ibm.com>
2016-02-29 10:37:53 -0600

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    {title: 'Harry Potter'},
+    {title: 'JavaScript the Good Parts'},
+    {title: 'The Curious Incident of the Dog in the Night Time'},
+    {title: 'The Diamond Age'},
+    {title: 'High Rise'}
+
+  ]
+}
